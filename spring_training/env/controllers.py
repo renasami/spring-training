44ed79d7c645bcaf3a02c667e693594cd19e5ec3
ren <drrr0502@gmail.com>
2021-03-02 16:53:28 +0900

    second

--- spring_training/env/controllers.py
+++ spring_training/env/controllers.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from starlette.templating import Jinja2Templates 
 from starlette.requests import Request
+import db
+from models import User,Task
  
 app = FastAPI(
     title='FastAPIでつくるtoDoアプリケーション',
@@ -16,7 +18,14 @@ def index(request: Request):
     return templates.TemplateResponse('index.html',
                                     {'request': request} )
 
-def admin(request:Request):
+def admin(request: Request):
+    # ユーザとタスクを取得
+    # とりあえず今はadminユーザのみ取得
+    user = db.session.query(User).filter(User.username == 'admin').first()
+    task = db.session.query(Task).filter(Task.user_id == user.id).all()
+    db.session.close()
+ 
     return templates.TemplateResponse('admin.html',
-                                    {'request':request,
-                                     'username':'admin'})
+                                      {'request': request,
+                                       'user': user,
+                                       'task': task})
